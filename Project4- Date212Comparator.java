c6c422fa000df7dc638cd5b3deec0c1fe9e28e31
SoraDavis <37783340+SoraDavis@users.noreply.github.com>
2018-03-26 11:35:13 -0400

    Create Project4- Date212Comparator.java

---
+++ Project4- Date212Comparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+
+public class Date212Comparator implements Comparator<Integer> {
+
+	//Use compareTo function in Date212_1 class to compare the two dates
+    public int compare(Integer date1, Integer date2) {
+        
+        return date1.compareTo(date2);
+    }
+
+}
