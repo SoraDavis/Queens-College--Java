19cbe80e8cbf17eee12895673f2dba21e0024435
SoraDavis <37783340+SoraDavis@users.noreply.github.com>
2018-03-26 11:31:16 -0400

    Create Project3.java

---
+++ Project3.java
@@ -0,0 +1,17 @@
+
+public class Project3 extends DateGUI1 {
+	
+	public static void main(String[] args) 
+	{
+		try 
+		{
+		//create a new DateGUI1 so that constructor is called and GUI is created
+		DateGUI1 DateGUI = new DateGUI1();
+		}
+		
+		catch(Date212Exception dte)
+		{
+			System.out.println(dte);
+		}
+	}
+}
