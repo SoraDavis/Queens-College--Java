b0a13111eaf48e7acb43154c523e43dcf773b088
SoraDavis <37783340+SoraDavis@users.noreply.github.com>
2018-03-26 10:56:46 -0400

    Create Project2- DateNode.java

---
+++ Project2- DateNode.java
@@ -0,0 +1,14 @@
+
+public class DateNode {
+	
+	protected Date212 data;
+	protected DateNode next;
+	
+    public DateNode (Date212 d) 
+    {
+    	  this.data = d;
+          this.next = null;
+    }//constructor
+    	
+
+}
