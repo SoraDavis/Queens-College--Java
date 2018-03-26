58ffc40c979d291167352c401f9b812ec576ede7
SoraDavis <37783340+SoraDavis@users.noreply.github.com>
2018-03-26 11:12:39 -0400

    Update Project2- DateNode.java

--- Project2- DateNode.java
+++ Project2- DateNode.java
@@ -1,14 +1,19 @@
 
-public class DateNode {
-	
-	protected Date212 data;
+public class DateNode 
+{
+	protected Date212_1 data;
 	protected DateNode next;
-	
-    public DateNode (Date212 d) 
-    {
-    	  this.data = d;
-          this.next = null;
-    }//constructor
-    	
+
+	//constructor 1 takes in a Date212_1 object to use as data in Node
+	public DateNode(Date212_1 d) 
+	{
+		this.data = d;
+	}
+
+	//constructor 2 takes in a DateNode to point to as next
+	public DateNode(DateNode dN) 
+	{
+		this.next = dN;
+	}
 
 }
