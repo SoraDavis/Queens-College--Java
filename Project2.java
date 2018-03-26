e23b6a0a91691cc34d1153af44a093771080caf4
SoraDavis <37783340+SoraDavis@users.noreply.github.com>
2018-03-26 11:09:33 -0400

    Create Project2.java

---
+++ Project2.java
@@ -0,0 +1,9 @@
+
+public class Project2 extends DateGUI1 {
+	
+	public static void main(String[] args) 
+	{
+		//create a new DateGUI1 so that constructor is called and GUI is created
+		DateGUI1 DateGUI = new DateGUI1();
+	}
+}
